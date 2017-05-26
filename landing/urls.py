70095df2b726bd00454727ef7c17d5355fdbe313
MinICTDev1 <jakkiwa@gmail.com>
2017-05-26 16:05:28 +0300

    Add files via upload

--- landing/urls.py
+++ landing/urls.py
@@ -8,12 +8,17 @@ urlpatterns = [
     # ex: /landing/projects/
     url(r'^projects/$', views.projects, name='projects'),
     # ex: /landing/budget/
-    url(r'^budget/$', views.budget, name='budget'),
+    url(r'^budget/$', views.budget, name='budgets'),
     # ex: /landing/predict/
     url(r'^predict/$', views.predict, name='predict'),
     # ex: /landing/compare/
     url(r'^compare/$', views.compare, name='compare'),
     # ex: /landing/export/
     url(r'^export/$', views.export, name='export'),
-    url(r'^people/$', views.people, name='people'),
+    # for the project submenu's
+    url(r'^projects/ongoing/$', views.ongoing, name='ongoing'),
+    url(r'^projects/completed/$', views.completed, name='completed'),
+    url(r'^projects/stalled/$', views.stalled, name='stalled'),
+    url(r'^projects/future/$', views.future, name='future'),
+
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse, HttpResponseRedirect
+<<<<<<< HEAD
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+import django_tables2 as tables
+from django_tables2 import RequestConfig
+from .models import Budget, Project
+from .tables import BudgetTable, OngoingTable, CompletedTable, StalledTable, FutureTable
+
+=======
 from django.shortcuts import render
 import django_tables2 as tables
 
 from .models import Person
+>>>>>>> cfbe1180b99843f061f5215b4dcfa8c7a2779eb5
 
 # Create your views here.
 
@@ -13,7 +23,9 @@ def projects(request):
     return render(request, 'landing/projects.html')
 
 def budget(request):
-    return render(request, 'landing/budget.html')
+    table = BudgetTable(Budget.objects.all())
+    RequestConfig(request).configure(table)
+    return render(request, 'landing/budgets.html', {'table': table})
 
 def predict(request):
     return render(request, 'landing/predict.html')
@@ -24,5 +36,23 @@ def compare(request):
 def export(request):
     return render(request, 'landing/export.html')
 
+<<<<<<< HEAD
+def ongoing(request):
+    table = OngoingTable(Project.objects.filter(project_status='OG'))
+    return render(request, 'landing/ongoing.html', {'table': table})
+
+def completed(request):
+    table = CompletedTable(Project.objects.filter(project_status='CO'))
+    return render(request, 'landing/completed.html', {'table': table})
+
+def stalled(request):
+    table = StalledTable(Project.objects.filter(project_status='ST'))
+    return render(request, 'landing/stalled.html', {'table': table})
+
+def future(request):
+    table = FutureTable(Project.objects.filter(project_status='FR'))
+    return render(request, 'landing/future.html', {'table': table})
+=======
 def people(request):
     return render(request, 'landing/people.html', {'people': Person.objects.all()})
+>>>>>>> cfbe1180b99843f061f5215b4dcfa8c7a2779eb5
